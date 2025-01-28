7106225ce10a9266442490402207e537c66dc6aa
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2025-01-28 22:38:53 +0200

    test heroku

--- back-end/mirt_back/urls.py
+++ back-end/mirt_back/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('homeapp.urls')),
     path('hello/', include('hello_test.urls')),
 ]
